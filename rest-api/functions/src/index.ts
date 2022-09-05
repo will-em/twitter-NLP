c405d2dd37907bcda0fa7420351ad0dbe1d8f6b5
William Emanuelsson <wem@kth.se>
2022-09-06 00:43:49 +0200

    Working CORS

--- rest-api/functions/src/index.ts
+++ rest-api/functions/src/index.ts
@@ -1,9 +1,21 @@
 import * as functions from "firebase-functions";
+const cors = require("cors")({origin: true});
+const axios = require("axios").default;
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+  cors(request, response, () => {
+    axios.get("https://random-data-api.com/api/v2/users?size=2&is_xml=true")
+        .then((r) => {
+          response.send(r.data);
+        })
+        .catch((e) => {
+          response.sendStatus(e);
+        });
+  });
 });
