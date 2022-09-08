0ca091852fbbf118080fbe4a4892f93e06a2a79f
William Emanuelsson <wem@kth.se>
2022-09-09 00:43:43 +0200

    Working fetching of tweets

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  interface Tweet {
+    id: number;
+    text: string;
+  }
+
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+
+  useEffect(() => {
+    const apiUrl = "https://europe-west1-twitter-nlp-backend.cloudfunctions.net/getTweets?name=elonmusk";
+    axios.get(apiUrl).then((response: any) => {
+      console.log(response.data.data);
+      setTweets(response.data.data);
+    });
+  }, [])
+
+  const listTweets = tweets.map((tweet) => 
+    <p>{tweet.text}</p>
+  );
+
+  const loading = <p>Loading...</p>
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {listTweets.length > 0 ? listTweets : loading}
       </header>
     </div>
   );
